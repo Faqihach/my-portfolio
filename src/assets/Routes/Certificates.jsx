272b1c748565f1d86b42413aadd2f96d799d1ab6
Faqihach <faqihach1@gmail.com>
2025-07-09 14:42:57 +0700

    fix certificates

--- src/assets/Routes/Certificates.jsx
+++ src/assets/Routes/Certificates.jsx
@@ -1,13 +1,13 @@
-import Certificate1 from "../images/certificates/Certificate1.webp";
-import Certificate2 from "../images/certificates/Certificate2.webp";
-import Certificate3 from "../images/certificates/Certificate3.webp";
-import Certificate4 from "../images/certificates/Certificate4.webp";
-import Certificate5 from "../images/certificates/Certificate5.webp";
-import Certificate6 from "../images/certificates/Certificate6.webp";
-import Certificate7 from "../images/certificates/Certificate7.webp";
-import Certificate8 from "../images/certificates/Certificate8.webp";
-import Certificate9 from "../images/certificates/Certificate9.webp";
-import Certificate10 from "../images/certificates/Certificate10.webp";
+import Certificate1 from "../images/certificates/certificate1.webp";
+import Certificate2 from "../images/certificates/certificate2.webp";
+import Certificate3 from "../images/certificates/certificate3.webp";
+import Certificate4 from "../images/certificates/certificate4.webp";
+import Certificate5 from "../images/certificates/certificate5.webp";
+import Certificate6 from "../images/certificates/certificate6.webp";
+import Certificate7 from "../images/certificates/certificate7.webp";
+import Certificate8 from "../images/certificates/certificate8.webp";
+import Certificate9 from "../images/certificates/certificate9.webp";
+import Certificate10 from "../images/certificates/certificate10.webp";
 
 const certificateImages = [
 	Certificate1,
