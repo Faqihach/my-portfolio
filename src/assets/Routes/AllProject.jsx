b25a427e9fed65c6f1846a9012b84d770de0c0c6
Faqihach <faqihach1@gmail.com>
2025-07-09 13:30:44 +0700

    update project

--- src/assets/Routes/AllProject.jsx
+++ src/assets/Routes/AllProject.jsx
@@ -1,10 +1,29 @@
 import project1 from "../images/projects/project1.webp";
 import project2 from "../images/projects/project2.webp";
 import project3 from "../images/projects/project3.webp";
+import project4 from "../images/projects/project4.webp";
+import project5 from "../images/projects/project5.webp";
 const AllProject = [
 	{
+		title: "SIPERA (Sistem Informasi Pengelolaan Iuran Warga)",
+		img: project5,
+		description:
+			"A comprehensive web-based management system designed to simplify the management and payment of resident fees. SIPERA assists administrators in efficiently managing monthly fees, tracking payment history, generating financial reports, and maintaining financial transparency. Built with a modern Laravel framework and designed with Tailwind CSS for an optimal user experience.",
+		websiteUrl: "",
+		githubUrl: "https://github.com/Faqihach/SIPERA.git",
+		techStack: ["Laravel 12", "Tailwind", "MySQL", "Filament"],
+	},
+	{
+		title: "SIPEDAS (Sistem Informasi Perpustakaan Digital Anak Sekolah)",
+		img: project4,
+		description:
+			"A digital library management system specifically designed to meet the educational needs of school children. It is an intuitive platform for managing book collections and facilitating a digital reading experience for students. The system is equipped with features that make it easy for students to read directly and download for educational materials. Built with a modern Laravel framework and designed using Bootstrap.",
+		websiteUrl: "",
+		githubUrl: "https://github.com/Faqihach/SIPEDAS.git",
+		techStack: ["Laravel 10", "Bootstrap", "MySQL"],
+	},
+	{
 		title: "Web Developer Landing Page",
-
 		img: project1,
 		description:
 			"This landing page is designed as a profile page for a Web Developer. The page displays personal information, skills such as UI Design, Product Design, and Branding, as well as a list of projects and work experience statistics. The design uses a modern dark theme with a card and grid layout to convey information neatly.",
@@ -30,22 +49,6 @@ const AllProject = [
 		githubUrl: "https://github.com/Faqihach/startup-landing-page.git",
 		techStack: ["HTML5", "CSS3"],
 	},
-	// {
-	// 	title: "",
-	// 	img: "",
-	// 	description: "",
-	// 	websiteUrl: "",
-	// 	githubUrl: "",
-	// 	techStack: [],
-	// },
-	// {
-	// 	title: "",
-	// 	img: "",
-	// 	description: "",
-	// 	websiteUrl: "",
-	// 	githubUrl: "",
-	// 	techStack: [],
-	// },
 ];
 
 export default AllProject;
